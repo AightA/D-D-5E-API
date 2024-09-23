f74546d351f408c8f7753aa472b8b6576b8404fb
AightA <ahmed8merehil@yahoo.com>
2024-09-23 18:49:09 +0100

    edit package file

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename="/D-D-5E-API">
 			<App />
 		</BrowserRouter>
 	</StrictMode>
