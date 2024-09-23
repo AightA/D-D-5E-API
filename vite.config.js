686c0fd32c8f49c5a5586b52997fcd32bc8251a8
AightA <ahmed8merehil@yahoo.com>
2024-09-23 18:24:31 +0100

    prep for deploy on github

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: 'https://github.com/AightA/D-D-5E-API.git',
 	plugins: [react()],
 	test: {
 		globals: true,
